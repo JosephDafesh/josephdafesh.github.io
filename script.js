852ad3f3770667bb44eb5a22986d93e2e4d050a9
JosephDafesh <148285698+JosephDafesh@users.noreply.github.com>
2023-10-22 23:52:18 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+title2.style.opacity = 0;
 document.addEventListener('DOMContentLoaded', function () {
-    const textToType = "Hey there. I'm Joe. Let's build.";
+    const textToType = "Hey there, I'm Joe. Let's build.";
     const typingContainer = document.getElementById('typing-container');
+    const title2 = document.getElementById('title2');
 
     function typeText(text, index) {
         if (index < text.length) {
@@ -10,19 +12,16 @@ document.addEventListener('DOMContentLoaded', function () {
                 typeText(text, index);
             }, 100);
         } else {
-            typingContainer.style.transition = "opacity 5.5s"; 
+            typingContainer.style.transition = "opacity 6s";
             typingContainer.style.opacity = 0;
+
+            setTimeout(function () {
+                title2.style.transition = "opacity 3s";
+                title2.style.opacity = 1;
+            }, 5000); 
         }
     }
     typeText(textToType, 0);
 });
-
-
-
-
-
-
-
-
 
 
